9969e99d4b82067f3b5bd66a7880d674c649d00d
Picheri Likitha <121708442+Likithasowji-500k@users.noreply.github.com>
2024-04-03 15:18:42 +0530

    Add files via upload

---
+++ newtons_method.py
@@ -0,0 +1,21 @@
+import math
+x=[]
+a=int(input())
+x.append(a)
+k=1
+e=10**(-3)
+def f(x):
+ return x**2+(54/x)
+def f1(x):
+ return (2*x-(54/x**2))
+def f2(x):
+ return 2+(108/x**3)
+i=0
+print("finding the minimum for newtons method upto 3 iterations")
+while(i<3):
+ b= x[i]-(f1(x[i])/f2(x[i]))
+ x.append(b)
+ print("x[0]:",x[i]," | f1(x[i]):: ",f1(x[i]), " | f2(x[i])::",f2(x[i]
+), " |x[i+1] ::", x[i+1])
+ i=i+1
+print("the minimum lies in ", x[-1])
